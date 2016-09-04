0dc24bbf9798ecf56a77e198a121e9ff088425a6
fershopls <shaaf4ever@gmail.com>
2016-09-04 11:07:41 -0500

    Frontend added

---
+++ frontend/js/index.js
@@ -0,0 +1,5 @@
+url = "http://codepen.io/fershopls/pen/zBmWpR.html"
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', url, function() {
+  console.log('callback - particles.js config loaded');
+});
